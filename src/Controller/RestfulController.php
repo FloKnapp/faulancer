6d24969521cd5945261b4b14a434fdb48af4ad79
Flo Knapp <office@florianknapp.de>
2017-04-01 15:54:51 +0200

    Initial commit of restful api controller

---
+++ src/Controller/RestfulController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Class RestfulController | RestfulController.php
+ * @package Faulancer\Controller
+ * @author  Florian Knapp <office@florianknapp.de>
+ */
+namespace Faulancer\Controller;
+
+/**
+ * Class RestfulController
+ */
+abstract class RestfulController extends Controller
+{
+
+    public function get() {}
+
+    public function getList() {}
+
+    public function create() {}
+
+    public function update() {}
+
+    public function delete() {}
+
+}
