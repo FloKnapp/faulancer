fc9832ffa6a181003afaf11a9be1cad66d05619e
Flo Knapp <office@florianknapp.de>
2017-12-03 21:59:07 +0100

    Add user agent to request

--- src/Http/Adapter/Curl.php
+++ src/Http/Adapter/Curl.php
@@ -30,6 +30,8 @@ class Curl extends AbstractAdapter
             curl_setopt($ch, CURLOPT_POSTFIELDS, $request->getBody());
         }
 
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0');
+
         $this->response = curl_exec($ch);
         curl_close($ch);
     }
